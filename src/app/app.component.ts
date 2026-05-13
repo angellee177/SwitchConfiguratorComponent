f2ed6f73b962d71099d4991c36c13cccaae6a5cb
angellee177 <angel@paycombat.com>
2026-05-13 10:42:24 +0800

    refactor: project to follow angular clean architecture

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { SwitchPlateConfiguratorComponent } from './switch-plate-configurator/switch-plate-configurator.component';
+import { SwitchPlateConfiguratorComponent } from './components/switch-plate-configurator.component';
 
 @Component({
   selector: 'app-root',
